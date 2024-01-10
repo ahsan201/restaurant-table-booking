137c1011ac09240b7f890133b5fd6005b2ecf62b
Ahsan Habib Niloy <habibniloy500@gmail.com>
2024-01-10 02:18:54 -0800

    Major Update done with many changes ...left with some bug fix

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const year = currentDate.getFullYear();
 const lastDate = new Date(year, month + 1, 0).getDate();
 const hour = currentDate.getHours();
 const minute = currentDate.getMinutes();
+console.log(hour);
+console.log(minute);
 // console.log(minute);
 const genarateMonth = function () {
   const monthList = [
@@ -48,7 +50,7 @@ const genarateDates = function () {
     );
   }
 };
-
+// time condition
 const timeCondition = function () {
   const timeDropdown = document.querySelector(".times");
   const timeDropdownContents = timeDropdown.querySelectorAll("p");
@@ -57,8 +59,12 @@ const timeCondition = function () {
     const timeText = x.textContent;
     const hourText = Number(timeText.substring(0, 2));
     const minuteText = Number(timeText.substring(3, 5));
-    if (hourText <= hour && minuteText <= minute) {
+
+    if (hourText <= hour) {
       x.classList.add("disable");
+      if (hourText == hour && minuteText > minute) {
+        x.classList.toggle("disable");
+      }
     }
   });
 };
@@ -111,12 +117,76 @@ timeParagraphElements.forEach(function (paragraph) {
 // Table selection
 const tableList = document.querySelector(".tableList");
 const table = tableList.querySelectorAll("li");
+const selectedTableList = new Set();
 
 table.forEach(function (tableIteam) {
   tableIteam.addEventListener("click", function (event) {
     const targetTable = event.target.parentNode.children;
+    const tableName = event.target.parentNode.previousElementSibling;
     for (let i = 0; i < targetTable.length; i++) {
       targetTable[i].classList.toggle("tableSelected");
     }
+    if (selectedTableList.has(tableName.textContent)) {
+      selectedTableList.delete(tableName.textContent);
+    } else {
+      selectedTableList.add(tableName.textContent);
+    }
   });
 });
+// book now button
+// form -
+const firstName = document.getElementById("firstName");
+
+const lastName = document.getElementById("lastName");
+const phone = document.getElementById("phone");
+const email = document.getElementById("email");
+
+const getDate = document.getElementById("dateLable");
+const getMonth = document.getElementById("monthLable");
+const getTime = document.getElementById("timeLable");
+
+// submit popup -
+const bookNowBTN = document.getElementById("btn");
+const overLay = document.querySelector(".overlay");
+const popupContainer = document.querySelector(".popupContainer");
+const btnClose = document.getElementById("btnClose");
+// texts
+const nameText = document.getElementById("name");
+const tableText = document.getElementById("tableText");
+const dateText = document.getElementById("dateText");
+const monthText = document.getElementById("monthText");
+const timeText = document.getElementById("timeText");
+const emailText = document.getElementById("emailText");
+const phoneText = document.getElementById("phoneText");
+
+bookNowBTN.addEventListener("click", function (e) {
+  e.preventDefault();
+  let tableListArray = [...selectedTableList].join(", ");
+  nameText.textContent = firstName.value;
+  tableText.textContent = tableListArray;
+  console.log(tableListArray);
+  dateText.textContent = getDate.textContent;
+  monthText.textContent = getMonth.textContent;
+  timeText.textContent = getTime.textContent;
+  emailText.textContent = email.value;
+  phoneText.textContent = phone.value;
+
+  overLay.classList.remove("diplayClass");
+  popupContainer.classList.remove("diplayClass");
+});
+const closeAndReset = function () {
+  firstName.value = "";
+  lastName.value = "";
+  phone.value = "";
+  email.value = "";
+  tableText.textContent = "";
+  getDate.textContent = "Date";
+  getMonth.textContent = "Month";
+  getTime.textContent = "Time";
+
+  overLay.classList.add("diplayClass");
+  popupContainer.classList.add("diplayClass");
+};
+
+btnClose.addEventListener("click", closeAndReset);
+overLay.addEventListener("click", closeAndReset);
